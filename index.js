935ffb3acbbe48e0faf2f2ac0b7a73b3c1461e02
Ramashish <ramashishkush>
2022-04-02 23:20:50 +0530

    added start

--- index.js
+++ index.js
@@ -102,8 +102,8 @@ app.use((err, req, res, next) => {
     const { message = 'Something went wrong', statusCode = 500 } = err;
     res.status(statusCode).render('opports/error', { message });
 })
-
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 })
 
